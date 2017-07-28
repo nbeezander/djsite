7883a09210c5821eaee2b3067684c48fd1225a4f
wgtamg1@gmail.com <gh948264>
2017-07-28 19:13:12 +0800

    2017/7/28 some

--- mining/models.py
+++ mining/models.py
@@ -4,8 +4,8 @@ from django.db import models
 # Create your models here.
 
 class Type(models.Model):
-    name = models.CharField(max_length=16, verbose_name="type name")
-    intro = models.CharField(max_length=16, verbose_name="introduce")
+    name = models.CharField(max_length=16, verbose_name="分类名称")
+    intro = models.CharField(max_length=16, verbose_name="简介")
 
     def __str__(self):
         return self.name
@@ -13,14 +13,14 @@ class Type(models.Model):
 
 class Resource(models.Model):
     # 资源类；用来收集与呈现
-    name = models.CharField(max_length=32, verbose_name="resource name")
-    intro = models.CharField(max_length=64, verbose_name="introduce")
-    link = models.CharField(max_length=128, verbose_name="link")
-    doc = models.CharField(max_length=2048, verbose_name="doc", null=True)
-    note = models.CharField(max_length=2048, verbose_name="note", null=True)
-    example = models.CharField(max_length=2048, verbose_name="e.g.", null=True)
-    type = models.ForeignKey(Type, verbose_name="type")
-    inTime = models.DateTimeField(verbose_name='in time', auto_created=True)
+    name = models.CharField(max_length=32, verbose_name="名称")
+    intro = models.CharField(max_length=64, verbose_name="简介")
+    link = models.CharField(max_length=128, verbose_name="链接")
+    doc = models.TextField(max_length=2048, verbose_name="文档", null=True)
+    note = models.CharField(max_length=2048, verbose_name="笔记", null=True)
+    example = models.CharField(max_length=2048, verbose_name="示例", null=True)
+    type = models.ForeignKey(Type, verbose_name="类型")
+    inTime = models.DateTimeField(verbose_name='收录日期', auto_created=True)
 
     def __str__(self):
         return self.name
